e8533acb3234f9f12d50a994674d5b99d18c3bfa
Jenifer Otero <jeniferoteroguillen@gmail.com>
2023-01-26 18:31:50 -0600

    fetch-Api

---
+++ index.js
@@ -0,0 +1,68 @@
+let buttonGetData = document.getElementById("get-data");
+      let sectionTables = document.getElementById("tables");
+      let spinnerRef = document.getElementById("spinner");
+
+      buttonGetData.addEventListener("click", showDataDOM);
+      async function showDataDOM() {
+        spinnerRef.classList.remove("d-none");
+        let dataAllOptions = await checkTime();
+        if (dataAllOptions) {
+          sectionTables.innerHTML = `
+          <table class="table table-striped table-hover">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Avatar</th>
+            <th>ID</th>
+          </tr>
+        </thead>
+      <tbody>
+        ${dataAllOptions.map(mapperData).join("")}
+          </tbody>
+          </table>
+          `;
+          spinnerRef.classList.add("d-none");
+        }
+      }
+      function mapperData(item) {
+        return `<tr>
+                  <td>${item.first_name} ${item.last_name}</td>
+                  <td>${item.email}</td>
+                  <td><img class='img-thumbnail rounded-circle' src=${item.avatar}></td>
+                  <td>${item.id}</td>
+              </tr>`;
+      }
+      async function getData() {
+        try {
+          const data = await fetch("https://reqres.in/api/users?delay=3");
+          const parseData = await data.json();
+          return parseData.data;
+        } catch (error) {
+          console.error(error, "Fetch Failure");
+        }
+      }
+      function getMinutesFuture() {
+        let date = new Date();
+        date.setMinutes(date.getMinutes() + 1);
+        return date;
+      }
+      async function newData() {
+        let data = await getData();
+        localStorage.setItem("data", JSON.stringify(data));
+        localStorage.setItem("time", getMinutesFuture());
+        return getDataFromLocalStorage();
+      }
+      function getDataFromLocalStorage() {
+        return JSON.parse(localStorage.getItem("data"));
+      }
+      function checkTime() {
+        let storedTime = new Date(localStorage.getItem("time"));
+        let currentTime = new Date();
+        if (currentTime > storedTime) {
+          localStorage.removeItem("data");
+          localStorage.removeItem("time");
+          return newData();
+        }
+        return getDataFromLocalStorage();
+      }
